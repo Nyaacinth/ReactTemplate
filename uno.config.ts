76aa4dfd9cdaac632d3d930f1059e2552cef4983
Nyaacinth <80994270+Nyaacinth@users.noreply.github.com>
2023-02-08 11:29:51 +0800

    Add Transformer: Variant Groups

--- uno.config.ts
+++ uno.config.ts
@@ -1,8 +1,10 @@
 import presetRemToPx from "@unocss/preset-rem-to-px"
 import presetWind from "@unocss/preset-wind"
+import transformerVariantGroup from "@unocss/transformer-variant-group"
 
 import { defineConfig } from "unocss"
 
 export default defineConfig({
-    presets: [presetWind(), presetRemToPx()]
+    presets: [presetWind(), presetRemToPx()],
+    transformers: [transformerVariantGroup()]
 })
